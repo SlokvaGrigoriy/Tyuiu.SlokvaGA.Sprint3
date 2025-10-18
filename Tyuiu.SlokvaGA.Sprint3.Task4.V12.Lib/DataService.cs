64f6d70d59a0466dfeeb01d50005e7295d4fa16a
Григорий Слоква <Tyuiu.SlokvaGA@outlook.com>
2025-10-18 11:11:29 +0500

    Task4V12

---
+++ Tyuiu.SlokvaGA.Sprint3.Task4.V12.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SlokvaGA.Sprint3.Task4.V12.Lib
+{
+    public class DataService : ISprint3Task4V12
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 0;
+            for (int x = startValue; x <= stopValue; x++)
+                if (x == 0)
+                    break;
+                else
+                    res += x / (x + Math.Sin(x)) + 2.5;
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SlokvaGA.Sprint3.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.SlokvaGA.Sprint3.Task4.V12.Lib;
+
+namespace Tyuiu.SlokvaGA.Sprint3.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 17.156;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
