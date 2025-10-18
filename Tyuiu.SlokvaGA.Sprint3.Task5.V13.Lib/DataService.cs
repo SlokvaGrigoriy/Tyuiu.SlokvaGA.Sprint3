de4a8329c5045f11d262e277abf5c54ecf8d8dc7
Григорий Слоква <Tyuiu.SlokvaGA@outlook.com>
2025-10-18 11:58:24 +0500

    Task5V13

--- Tyuiu.SlokvaGA.Sprint3.Task5.V13.Lib/DataService.cs
+++ Tyuiu.SlokvaGA.Sprint3.Task5.V13.Lib/DataService.cs
@@ -2,7 +2,7 @@
 
 namespace Tyuiu.SlokvaGA.Sprint3.Task5.V13.Lib
 {
-    public class DataService : ISprint3Task5V12
+    public class DataService : ISprint3Task5V13
     {
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
